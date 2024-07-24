983b05c2bfc02a8285337d88cba40a95a7b279c0
Himanshu Chandna <himanshuchandna0007@gmail.com>
2024-07-24 11:10:45 +0530

    favicon add

---
+++ build/dev/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="/build/media/favicon/ATMOS AQ Logo (96 x 96 px).svg">
+    <title>AtmosAQ</title>
+</head>
+<body>
+    
+</body>
+</html>
